3d5c4f11fd78202bcb38333e5ef25db0eed1c1f6
User1 <User1@RDV-005604>
2019-10-09 19:26:57 -0400

    added new class called Movie

---
+++ Week03/WpfApplication1/WpfApplication1/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
+{
+    public class Movie
+    {
+
+        public string Title;
+        public int ReleaseYear;
+
+
+        public Movie(string title, int releaseyear)
+        {
+            Title = title;
+            ReleaseYear = releaseyear;
+        }
+
+
+
+
+        public void ShowDetails()
+        {
+            MessageBox.Show(Title + " was released in : " + ReleaseYear );
+        }
+        
+
+
+    }
+}
