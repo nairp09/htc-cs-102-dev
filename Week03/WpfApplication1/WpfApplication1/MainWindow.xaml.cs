a0001f63b0b2f74342fea5584498ae1c0be371d2
User1 <User1@RDV-005604>
2019-10-16 19:27:52 -0400

    changed wpf app

--- Week03/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ Week03/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Movie> Movies = new List<Movie>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Movie movie in Movies)
+            {
+                movie.ShowDetails();
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string title = titleInput.Text;
+            int  releaseYear = Convert.ToInt32(releaseYearInput.Text);
+            string director = directorInput.Text;
+            string genre = genreInput.Text;
+            string length = lengthInput.Text;
+
+            Movie movie = new Movie(title, releaseYear, director,length,genre);
+            Movies.Add(movie);
+            MessageBox.Show("Movie was added!");
         }
     }
 }
--- Week03/WpfApplication1/WpfApplication1/Movie.cs
+++ Week03/WpfApplication1/WpfApplication1/Movie.cs
@@ -12,12 +12,19 @@ namespace WpfApplication1
 
         public string Title;
         public int ReleaseYear;
+        public string Director;
+        public string Length;
+        public string Genre;
 
 
-        public Movie(string title, int releaseyear)
+        public Movie(string title, int releaseyear, string director, string length, string genre)
         {
             Title = title;
             ReleaseYear = releaseyear;
+            Director = director;
+            Length = length;
+            Genre = genre;
+
         }
 
 
@@ -25,7 +32,7 @@ namespace WpfApplication1
 
         public void ShowDetails()
         {
-            MessageBox.Show(Title + " was released in : " + ReleaseYear );
+            MessageBox.Show(Title + " was released in : " + ReleaseYear + " ,the director was " + Director + ",it is " + Length + " hours long, and it is in the category of " + Genre  );
         }
         
 
